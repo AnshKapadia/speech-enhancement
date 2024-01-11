320c802026e218a609b3a935bf9d937d91b1671c
AnshKapadia <82949517+AnshKapadia@users.noreply.github.com>
2024-01-11 12:16:16 +0530

    fixed path for .wav file

--- code/Spectrogram_matplotlib.py
+++ code/Spectrogram_matplotlib.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import scipy as sp
 
 #converting .wav audio file to numpy array
-fs, aud = sp.io.wavfile.read('whistle_1.wav')
+fs, aud = sp.io.wavfile.read('..\data\whistle_1.wav')
 #computing the stft of our audio clip as well as plotting the spectrogram
 stft, freq, time, spec = plt.specgram(aud,Fs = fs,cmap='magma')
 plt.xlabel('Time')
