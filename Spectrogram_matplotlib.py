76908c1917418e4a420f43e1cb2eaf6b1a81a00d
AnshKapadia <82949517+AnshKapadia@users.noreply.github.com>
2024-01-11 12:08:35 +0530

    Add files via upload

---
+++ Spectrogram_matplotlib.py
@@ -0,0 +1,14 @@
+#Necessary imports 
+import  numpy  as  np
+import matplotlib.pyplot as plt
+import scipy as sp
+
+#converting .wav audio file to numpy array
+fs, aud = sp.io.wavfile.read('whistle_1.wav')
+#computing the stft of our audio clip as well as plotting the spectrogram
+stft, freq, time, spec = plt.specgram(aud,Fs = fs,cmap='magma')
+plt.xlabel('Time')
+plt.ylabel('Frequency')
+plt.colorbar()
+plt.show()
+
