850e5f7ed99efce3ad2035ada7106481949e6554
AnshKapadia <82949517+AnshKapadia@users.noreply.github.com>
2024-01-11 12:21:29 +0530

    fixed formatting of code

--- code/Spectrogram_matplotlib.py
+++ code/Spectrogram_matplotlib.py
@@ -5,6 +5,7 @@ import scipy as sp
 
 #converting .wav audio file to numpy array
 fs, aud = sp.io.wavfile.read('..\data\whistle_1.wav')
+
 #computing the stft of our audio clip as well as plotting the spectrogram
 stft, freq, time, spec = plt.specgram(aud,Fs = fs,cmap='magma')
 plt.xlabel('Time')
